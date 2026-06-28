a4f60f96c314a90e597c48e9e391eb475d059c0a
S557s179 <shrishiva0824@gmail.com>
2026-06-28 14:46:34 -0500

    Update config.py

--- project1/config.py
+++ project1/config.py
@@ -1,4 +1,6 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # IMPORTANT: JWT requires this exact key name
     JWT_SECRET_KEY = 'super-secret-key'
