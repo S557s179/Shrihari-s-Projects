5a760f7c6c29da92e9da5d2f1ea534a4e10317ae
S557s179 <shrishiva0824@gmail.com>
2026-06-28 15:09:01 -0500

    Update auth_utils.py

--- project1/auth_utils.py
+++ project1/auth_utils.py
@@ -1,16 +1,7 @@
 import bcrypt
 
-
 def hash_password(password):
-    hashed = bcrypt.hashpw(
-        password.encode('utf-8'),
-        bcrypt.gensalt()
-    )
-    return hashed.decode('utf-8')
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
-
-def check_password(password, hashed_password):
-    return bcrypt.checkpw(
-        password.encode('utf-8'),
-        hashed_password.encode('utf-8')
-    )
+def check_password(password, hashed):
+    return bcrypt.checkpw(password.encode(), hashed.encode())
