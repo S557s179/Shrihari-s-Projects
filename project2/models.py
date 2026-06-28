60d5fb8ba0b18704606b64eb90b33ef5bb4b7b7a
S557s179 <shrishiva0824@gmail.com>
2026-06-28 17:05:23 -0500

    Update models.py

--- project2/models.py
+++ project2/models.py
@@ -2,17 +2,44 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+
+    username = db.Column(
+        db.String(80),
+        unique=True,
+        nullable=False
+    )
+
+    password = db.Column(
+        db.String(200),
+        nullable=False
+    )
+
 
 class URL(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
 
-    long_url = db.Column(db.String(500), nullable=False)
-    short_code = db.Column(db.String(10), unique=True, nullable=False)
+    long_url = db.Column(
+        db.String(500),
+        nullable=False
+    )
 
-    clicks = db.Column(db.Integer, default=0)
+    short_code = db.Column(
+        db.String(10),
+        unique=True,
+        nullable=False
+    )
 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    clicks = db.Column(
+        db.Integer,
+        default=0
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id")
+    )
