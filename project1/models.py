ed09529107b356f26bca887de10e0f7c26849058
S557s179 <shrishiva0824@gmail.com>
2026-06-28 14:47:44 -0500

    Update models.py

--- project1/models.py
+++ project1/models.py
@@ -2,44 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
 
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    username = db.Column(
-        db.String(80),
-        unique=True,
-        nullable=False
-    )
+    username = db.Column(db.String(80), unique=True, nullable=False)
 
-    password = db.Column(
-        db.String(200),
-        nullable=False
-    )
+    password = db.Column(db.String(200), nullable=False)
+
+    # optional improvement
+    problems = db.relationship('Problem', backref='user', lazy=True)
+
 
 class Problem(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
 
-    title = db.Column(
-        db.String(200),
-        nullable=False
-    )
+    title = db.Column(db.String(200), nullable=False)
 
-    difficulty = db.Column(
-        db.String(50)
-    )
+    difficulty = db.Column(db.String(50))
 
-    topic = db.Column(
-        db.String(100)
-    )
+    topic = db.Column(db.String(100))
 
-    completed = db.Column(
-        db.Boolean,
-        default=False
-    )
+    completed = db.Column(db.Boolean, default=False)
 
-    user_id = db.Column(
-        db.Integer,
-        db.ForeignKey('user.id')
-    )
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
