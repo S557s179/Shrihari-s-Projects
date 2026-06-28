aa6764324ccf8c7aacec043f4b70baf1d68fa76a
S557s179 <shrishiva0824@gmail.com>
2026-06-28 14:41:45 -0500

    Update app.py

--- project1/app.py
+++ project1/app.py
@@ -9,25 +9,22 @@ from routes.problem_routes import problem_bp
 
 app = Flask(__name__)
 
+# Load config
 app.config.from_object(Config)
 
+# Initialize DB
 db.init_app(app)
 
+# JWT setup
 jwt = JWTManager(app)
 
-# Register routes
+# Register blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(problem_bp)
+
 
 @app.route('/')
 def home():
     return {
         "message": "Interview Prep Tracker API"
     }
-
-if __name__ == '__main__':
-
-    with app.app_context():
-        db.create_all()
-
-    app.run(debug=True)
