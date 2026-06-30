3078ade1e4737d1b1e0ec973864871ff739942a1
S557s179 <shrishiva0824@gmail.com>
2026-06-30 17:54:46 -0500

    Create TransactionRepository.java

---
+++ project 5/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.bank.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByFromAccountOrToAccount(String from, String to);
+}
